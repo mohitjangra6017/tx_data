bbc3540e311e828d0903687c37925612df0515cb
Qingyang Liu <qingyang.liu@totaralearning.com>
2020-11-18 13:15:54 +1300

    TL-27688 totara_competency: Remove remaining references to totara_competency block

--- server/lib/classes/plugin_manager.php
+++ server/lib/classes/plugin_manager.php
@@ -1224,7 +1224,6 @@ class core_plugin_manager {
                 'totara_recently_viewed', 'totara_recommendations',
                 'course_search',
                 'admin_subnav', 'admin_related_pages', 'course_navigation',
-                'totara_competency',
             ),
 
             'booktool' => array(
