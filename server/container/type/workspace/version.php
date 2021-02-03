5f37f41ef34ba7c64386118161bf7dafdcb62d82
Qingyang Liu <qingyang.liu@totaralearning.com>
2021-02-04 09:47:09 +1300

    TL-29422 container_workspace: Create a notification for a new discussion posted in the workspace

--- server/container/type/workspace/version.php
+++ server/container/type/workspace/version.php
@@ -25,7 +25,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /* NOTE: the following version number must be bumped during each major or minor Totara release. */
 
-$plugin->version  = 2020121600;                 // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2020121601;                 // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2020101200;                 // Requires this Totara version.
 $plugin->component = 'container_workspace';          // To check on upgrade, that module sits in correct place
 
