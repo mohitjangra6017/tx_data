9290c84e3b9efef1268ee8bfc70f0ed449d6f02c
Petr Skoda <petr.skoda@totaralearning.com>
2021-04-12 08:34:12 +1200

    TL-30347 dml: fix last error display for PostgreSQL

--- server/lib/dml/pgsql_native_moodle_database.php
+++ server/lib/dml/pgsql_native_moodle_database.php
@@ -43,6 +43,9 @@ class pgsql_native_moodle_database extends moodle_database {
     /** @var resource $pgsql database resource */
     protected $pgsql     = null;
 
+    /** @var null string */
+    private $last_error = null;
+
     protected $last_error_reporting; // To handle pgsql driver default verbosity
 
     /** @var bool savepoint hack for MDL-35506 - workaround for automatic transaction rollback on error */
@@ -265,6 +268,7 @@ class pgsql_native_moodle_database extends moodle_database {
         parent::query_start($sql, $params, $type, $extrainfo);
         // pgsql driver tents to send debug to output, we do not need that ;-)
         $this->last_error_reporting = error_reporting(0);
+        $this->last_error = null;
     }
 
     /**
@@ -277,6 +281,8 @@ class pgsql_native_moodle_database extends moodle_database {
         error_reporting($this->last_error_reporting);
 
         if ($result === false) {
+            $this->last_error = pg_last_error($this->pgsql);
+
             // Totara: for consistency with other DBs we return to pre-error savepoint, we need to do this before db logging in parent method.
             if ($this->savepointpresent) {
                 $res = @pg_query($this->pgsql, "ROLLBACK TO SAVEPOINT moodle_pg_savepoint; SAVEPOINT moodle_pg_savepoint");
@@ -295,6 +301,7 @@ class pgsql_native_moodle_database extends moodle_database {
                 pg_free_result($res);
             }
         }
+        $this->last_error = null;
     }
 
     /**
@@ -336,10 +343,10 @@ class pgsql_native_moodle_database extends moodle_database {
 
     /**
      * Returns last error reported by database engine.
-     * @return string error message
+     * @return string|null error message
      */
     public function get_last_error() {
-        return pg_last_error($this->pgsql);
+        return $this->last_error;
     }
 
     /**
